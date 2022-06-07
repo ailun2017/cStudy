34c58c3fb8dd31ceac7ff5e8625c7e3636a5a887
jack.aliyun <jxhczal@163.com>
2022-06-07 17:31:50 +0800

    modify makefile

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zal/jack.lin.code
+# Install script for directory: /home/jack/cStudy
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zal/jack.lin.code/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jack/cStudy/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
